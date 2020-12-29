43a0bee99b57cac7d824cbff379a9c8d9d9c77cf
Lee Watson <rev@revthefox.co.uk>
2020-12-29 21:38:34 +0000

    Initial commit

---
+++ spongebob.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+from contextlib import closing
+import random
+from sys import stdin
+import subprocess
+import os
+
+
+def xclip(*args, data=None):
+    base_command = ["xclip", "-selection", "CLIPBOARD", *args]
+
+    if "-o" in args:
+        with closing(
+            subprocess.Popen(base_command, stdout=subprocess.PIPE).stdout
+        ) as stdout:
+            return stdout.read().decode("UTF-8")
+    else:
+        with closing(
+            subprocess.Popen(base_command, stdin=subprocess.PIPE).stdin
+        ) as stdin:
+            stdin.write(data.encode("UTF-8"))
+
+
+def notify(text):
+    subprocess.Popen(["notify-send", os.path.basename(__file__), text])
+
+
+def get_clipboard_text():
+    return xclip("-o")
+
+
+def set_clipboard_text(data):
+    xclip(data=data)
+    notify(data)
+
+
+def main():
+    text = get_clipboard_text()
+    if not text:
+        notify("No text found in clipboard!")
+        return
+
+    text = "".join(
+        c.upper() if (i % 2 == 0 or random.random() <= 0.3) else c
+        for i, c in enumerate(text.lower())
+    )
+    set_clipboard_text(text)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as exc:
+        notify(type(exc).__name__)
+        raise exc
---
+++ zncreverse.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+import argparse
+import errno
+import logging as log
+import socket
+import subprocess
+import sys
+from ipaddress import ip_address
+
+
+def shell(command):
+    try:
+        subprocess.run(command.split())
+    except (FileNotFoundError, subprocess.SubprocessError) as exc:
+        log.error(exc)
+        sys.exit(1)
+
+
+def reload_zone(zone):
+    shell(f"rndc freeze {zone}")
+    shell(f"rndc reload {zone}")
+    shell(f"rndc thaw {zone}")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Add an IPv6 reverse PTR record for ZNC vhosts.", prog="zncreverse"
+    )
+    parser.add_argument("domain", help="Hostname to create a PTR record for")
+    parser.add_argument(
+        "-a", "--address", nargs="?", help="Address to use instead of querying DNS"
+    )
+    parser.add_argument(
+        "-z",
+        "--zone",
+        nargs="?",
+        default="3.0.0.6.0.0.0.0.3.5.7.0.6.2.ip6.arpa",
+        help="Zone name",
+    )
+    parser.add_argument("-o", "--output", nargs="?", help="Output file for PTR record")
+    return parser.parse_args()
+
+
+def get_ip_address(domain):
+    addrinfo = socket.getaddrinfo(domain, None, socket.AF_INET6)
+    return addrinfo[0][4][0]
+
+
+def get_reverse_ptr(address):
+    return ip_address(address).reverse_pointer
+
+
+def main():
+    args = parse_args()
+    domain = args.domain
+    zone = args.zone
+    zone_file = args.output or f"/var/lib/bind/{zone}.zone"
+
+    try:
+        address = args.address or get_ip_address(domain)
+        reverse_ptr = get_reverse_ptr(address)
+    except Exception as exc:
+        log.error(exc)
+        sys.exit(1)
+
+    ptr_record = f"{reverse_ptr}.    IN    PTR    {domain}.\n"
+    log.info(f"The following PTR record will be used for {domain} ({address})")
+    log.info(ptr_record)
+
+    try:
+        input("Press Enter to proceed...")
+    except KeyboardInterrupt:
+        return
+
+    try:
+        with open(f"{zone_file}", "a") as fd:
+            log.info(f"Writing to {zone_file}")
+            fd.write(f"{ptr_record}")
+            reload_zone(zone)
+    except FileNotFoundError as exc:
+        log.error(exc)
+        sys.exit(errno.ENOENT)
+
+
+if __name__ == "__main__":
+    log.basicConfig(level=log.INFO, format="%(message)s")
+    main()
