d10a3be22ab2f27fb52015af4b023026493574c8
Lee Watson <rev@revthefox.co.uk>
2021-01-12 20:22:36 +0000

    Make notify-user look for sockets only.

--- notify-user.sh
+++ notify-user.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-display=":$(find /tmp/.X11-unix/* | sed 's#/tmp/.X11-unix/X##' | head -n 1)"
+display=":$(find /tmp/.X11-unix/* -type s | sed 's#/tmp/.X11-unix/X##' | head -n 1)"
 x11_pid=$(pgrep 'Xorg')
 user=$(ps -o user= -p "$x11_pid")
 dbus_socket="/run/user/$(id -u "$user")/bus"
