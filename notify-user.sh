507b2ebdc38c100714b03815253439b42c0156ca
Lee Watson <rev@revthefox.co.uk>
2021-01-12 20:28:06 +0000

    Use machinectl for notify-send to avoid having to hunt for a dbus socket.

--- notify-user.sh
+++ notify-user.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-display=":$(find /tmp/.X11-unix/* -type s | sed 's#/tmp/.X11-unix/X##' | head -n 1)"
 x11_pid=$(pgrep 'Xorg')
 user=$(ps -o user= -p "$x11_pid")
-dbus_socket="/run/user/$(id -u "$user")/bus"
-
-sudo -u "$user" DISPLAY="$display" DBUS_SESSION_BUS_ADDRESS=unix:path="$dbus_socket" notify-send "$@"
+machinectl shell "$user"@ "$(which notify-send)" "$@"
