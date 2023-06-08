0293e48358e912301e010dd979db3fc368f9789f
Lee Watson <me@lwatson.dev>
2023-06-09 00:35:33 +0100

    notify-user.sh: formatting

--- notify-user.sh
+++ notify-user.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-user_pid=$((pgrep 'Xorg' || pgrep 'sway') | head -n1)
+user_pid=$( (pgrep 'Xorg' || pgrep 'sway') | head -n1 )
 user=$(ps -o user= -p "$user_pid")
 machinectl shell "$user"@ "$(which notify-send)" "$@"
