92fc6cb62d04c1a9b8ce549c45311c753f8cfef8
Lee Watson <me@lwatson.dev>
2025-01-27 11:58:50 +0000

    stuff and also switch to xdg-mime

--- wpste.fish
+++ wpste.fish
@@ -1,12 +1,12 @@
 #!/usr/bin/fish
 
 function _notify
-    command -q notify-send; and notify-send --app-name "wpste" $argv; or true
+    command -q notify-send; and notify-send --app-name wpste $argv; or true
 end
 
 function _log
-    set -l level $argv[1]
-    set -l message $argv[2..]
+    set -f level $argv[1]
+    set -f message $argv[2..]
 
     switch $level
         case INFO
@@ -32,21 +32,21 @@ function _log
 end
 
 function log_info
-    _log "INFO" $argv
+    _log INFO $argv
 end
 
 function log_warn
-    _log "WARN" $argv
+    _log WARN $argv
 end
 
 function log_error
-    set -l options \
+    set -f options \
         (fish_opt --short e --long exit --required-val)
 
     argparse --stop-nonopt $options -- $argv
     or return
 
-    _log "ERROR" $argv
+    _log ERROR $argv
     if set -q __notify_on_error
         _notify -u critical "wpste encountered an error." "$argv"
     end
@@ -57,7 +57,7 @@ function log_error
 end
 
 function log_debug
-    set -q WPSTE_DEBUG; and _log "DEBUG" $argv 1>&2
+    set -q WPSTE_DEBUG; and _log DEBUG $argv 1>&2
 end
 
 function _print_help
@@ -67,13 +67,13 @@ function _print_help
 end
 
 function _is_image
-    set -l file $argv[1]
-    set -l mimetype (mimetype -b "$file")
+    set -f file $argv[1]
+    set -f mimetype (xdg-mime query filetype "$file")
     string match "image/*" "$mimetype"
 end
 
 function _check_requirements
-    set -l required_commands curl mpv notify-send grimshot wl-copy swappy oxipng
+    set -f required_commands curl mpv notify-send grimshot wl-copy swappy oxipng
 
     if not set -q WAYLAND_DISPLAY
         log_error --exit 1 "WAYLAND_DISPLAY is not set. wpste expects a Wayland environment."
@@ -89,8 +89,8 @@ function _check_requirements
 end
 
 function _play_sound
-    set -l sound_file "camera-shutter.oga"
-    set -l sound_file "/usr/share/sounds/freedesktop/stereo/$sound_file"
+    set -f sound_file "camera-shutter.oga"
+    set -f sound_file "/usr/share/sounds/freedesktop/stereo/$sound_file"
 
     if test -f "$sound_file"
         mpv --no-video --no-terminal "$sound_file" &
@@ -100,27 +100,28 @@ function _play_sound
 end
 
 function _take_screenshot
-    set -l target $argv[1]
-    set -l save_path (mktemp --tmpdir --suffix=.png wpste_(date --iso-8601).XXXX)
+    set -f target $argv[1]
+    set -f save_path (mktemp --tmpdir --suffix=.png wpste_(date --iso-8601).XXXX)
 
-    set -l grimshot_output (grimshot save "$target" "$save_path" 2>&1)
-    set -l grimshot_status $status
+    set -f grimshot_output (grimshot save "$target" "$save_path" 2>&1)
+    set -f grimshot_status $status
     log_debug "grimshot: $grimshot_output"
     if not test $grimshot_status -eq 0
         if string match -q "selection cancelled" "$grimshot_output"
             # Capitalise the s, add a period.
-            log_info (string replace -r "^s" "S" "$grimshot_output."); exit 0
+            log_info (string replace -r "^s" "S" "$grimshot_output.")
+            exit 0
         end
         log_error --exit $grimshot_status "Grimshot error: $grimshot_output"
     end
 
     _play_sound
-    set -l oxipng_output (oxipng --strip safe "$save_path" 2>&1); and log_debug "oxipng: $oxipng_output"
+    set -f oxipng_output (oxipng --strip safe "$save_path" 2>&1); and log_debug "oxipng: $oxipng_output"
     echo "$save_path"
 end
 
 function _upload_file
-    set -l options \
+    set -f options \
         (fish_opt --short k --long key --required-val) \
         (fish_opt --short f --long file --required-val)
 
@@ -136,15 +137,15 @@ function _upload_file
     end
 
     log_debug "Uploading $_flag_f"
-    set -l response (curl -sSL -H "Authorization: Bearer $_flag_k" -F file="@$_flag_f" "$CONFIG_UPLOAD_URL" 2>&1)
-    set -l curl_status $status
+    set -f response (curl -sSL -H "Authorization: Bearer $_flag_k" -F file="@$_flag_f" "$CONFIG_UPLOAD_URL" 2>&1)
+    set -f curl_status $status
 
     test $curl_status -eq 0; or log_error --exit $curl_status "Upload failed: $response"
     echo "$response" | jq .url --raw-output; or log_error --exit $status "JSON parsing failed: $response"
 end
 
 function _copy_to_clipboard
-    set -l options \
+    set -f options \
         (fish_opt --short f --long file --required-val) \
         (fish_opt --short t --long text --required-val)
 
@@ -166,7 +167,7 @@ function _copy_to_clipboard
             # Hardcoded to image/png for now otherwise certain apps don't support pasting non-png images.
             # https://github.com/bugaevc/wl-clipboard/issues/8
             # https://github.com/bugaevc/wl-clipboard/issues/71
-            wl-copy --primary --type "image/png" < "$_flag_f"; or log_error --exit $status "Failed to copy $_flag_f to the clipboard."
+            wl-copy --primary --type image/png <"$_flag_f"; or log_error --exit $status "Failed to copy $_flag_f to the clipboard."
         else
             log_debug "$_flag_f is not an image. Not copying to clipboard."
         end
@@ -174,17 +175,17 @@ function _copy_to_clipboard
 end
 
 function _source_config
-    set -l config_file "$XDG_CONFIG_HOME/wpste/config"
+    set -f config_file "$XDG_CONFIG_HOME/wpste/config"
     if not test -f "$config_file"
         set -l default_config \
             "## API key for your pste instance. Required." \
             "# API_KEY=" \
             "## URL to your pste instance's upload endpoint. Optional." \
-            "# UPLOAD_URL=https://dev.pste.pw/api/upload"
+            "# UPLOAD_URL=https://pste.pw/api/upload"
 
         log_error "No config file found at $config_file. A default one will be created."
         mkdir -p "$(dirname "$config_file")"; or log_error --exit $status "Failed to create default config file."
-        echo -e "$(string join '\n' $default_config)" > "$config_file"
+        echo -e "$(string join '\n' $default_config)" >"$config_file"
         exit 1
     end
 
@@ -194,24 +195,23 @@ function _source_config
         set -g CONFIG_$item[1] $item[2]
     end
 
-    set -q CONFIG_UPLOAD_URL; or set -g CONFIG_UPLOAD_URL "https://dev.pste.pw/api/upload"
+    set -q CONFIG_UPLOAD_URL; or set -g CONFIG_UPLOAD_URL "https://pste.pw/api/upload"
 end
 
 function wpste_main
     _check_requirements
     _source_config
 
-    set -l options \
+    set -f options \
         (fish_opt --short h --long help) \
         (fish_opt --short c --long copy) \
         (fish_opt --short n --long notify) \
         (fish_opt --short e --long edit) \
-        (fish_opt --short t --long target --required-val) \
-
+        (fish_opt --short t --long target --required-val)
     argparse --name=wpste --stop-nonopt $options -- $argv
     or return
 
-    test (count $argv) -eq 1; and set -l _flag_f $argv[1]
+    test (count $argv) -eq 1; and set -f _flag_f $argv[1]
 
     log_debug "_flag_h: $_flag_h"
     log_debug "_flag_c: $_flag_c"
@@ -224,35 +224,36 @@ function wpste_main
     set -q _flag_n; and set -g __notify_on_error
 
     if set -q _flag_t
-        set -l valid_targets "active" "screen" "output" "area" "window"
+        set -l valid_targets active screen output area window
         if not contains "$_flag_t" $valid_targets
             set -l valid_targets (string join ", " $valid_targets)
             log_error --exit 2 -- "--target must be one of $valid_targets."
         end
     else
-        set -l _flag_t "output"
+        set -f _flag_t output
     end
 
     if not set -q _flag_f
-        set _flag_f (_take_screenshot "$_flag_t")
+        set -f _flag_f (_take_screenshot "$_flag_t")
     end
 
     if set -q _flag_e
-        if not _is_image "$_flag_f" > /dev/null
+        if not _is_image "$_flag_f" >/dev/null
             log_warn "Ignoring --edit because $_flag_f is not an image."
         else
             swappy -f "$_flag_f" -o "$_flag_f"; or log_error --exit $status "Editing image failed."
         end
     end
 
-    set -l url (_upload_file --file "$_flag_f" --key "$CONFIG_API_KEY")
+    set -f url (_upload_file --file "$_flag_f" --key "$CONFIG_API_KEY")
+    set url (string replace -r "^http:" "https:" "$url")
 
     if set -q _flag_c
         _copy_to_clipboard --text "$url" --file "$_flag_f"
         if set -q _flag_n
             set -l notification_title "URL copied to clipboard."
             set -l notification_args
-            if _is_image "$_flag_f" > /dev/null
+            if _is_image "$_flag_f" >/dev/null
                 set -p notification_title "Image and"
                 set notification_args --icon "$_flag_f"
             end
